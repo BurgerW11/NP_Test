bd39ed0bdf0988eb35c3229f83660c40fe41eed2
han.wei <han.wei@netprisma.com>
2025-07-24 17:59:53 +0800

    Updated Question 7

--- Number_7/main.c
+++ Number_7/main.c
@@ -1,8 +1,35 @@
+/* 
+
+        NOT COMPLETED YET
+        WORKING IN PROGRESS
+
+                NOT COMPLETED YET
+        WORKING IN PROGRESS
+
+                NOT COMPLETED YET
+        WORKING IN PROGRESS
+
+                NOT COMPLETED YET
+        WORKING IN PROGRESS
+
+                NOT COMPLETED YET
+        WORKING IN PROGRESS
+
+
+        SAMPLE OUTPUT :         i. [13.12.12:256] [ERROR] [filename.c:main():56] report error
+
+*/
+
+
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int main()
 {
 
-    printf ("This is from source file : %s, function : %s, line : %d, printing message : %s \n", __FILE__, __FUNCTION__, __LINE__ );
+    // const char *string = "hello world \n";
+    // printf ("This is from source file : %s, function : %s, line : %d, printing message : %s \n", __FILE__, __FUNCTION__, __LINE__, string);
     return 0;
 }
